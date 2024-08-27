260f2e57b2ca10f5478b220722011a0744cfd9ca
Trimitor <73941033+Trimitor@users.noreply.github.com>
2024-08-27 22:35:21 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const url = 'https://cchgeu.ru/studentu/schedule/stf/';
+const url = 'https://api.cors.lol/url=https://cchgeu.ru/studentu/schedule/stf/';
 
 fetch(url)
-  .then(response => response.text())
-  .then(html => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    const links = doc.querySelectorAll('a');
+    .then(response => response.text())
+    .then(html => {
+        console.log(html);
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const links = doc.querySelectorAll('a');
 
-    links.forEach(link => {
-      if (link.href.includes('bRSO_211.xlsx')) {
-        console.log('Найдена ссылка:', link.href);
-      }
+        links.forEach(link => {
+            if (link.href.includes('bRSO_211.xlsx')) {
+                console.log('Найдена ссылка:', link.href);
+            }
+        });
+    })
+    .catch(err => {
+        console.error('Ошибка при загрузке страницы:', err);
     });
-  })
-  .catch(err => {
-    console.error('Ошибка при загрузке страницы:', err);
-  });
